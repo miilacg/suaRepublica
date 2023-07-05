7f095aa8e6d90e605ed0eb9a1d0d96b0fbb68607
Camila Guimarães <camiilacguimaraes@gmail.com>
2023-07-05 11:06:22 -0300

    :sparkles: logoff

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,8 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 import { AuthContext } from '../config/store';
 
@@ -8,17 +11,34 @@ import '../style/Header.css';
 function Header({ titulo }) {
   const { auth } = useContext(AuthContext);
 
+  const [openMenu, setOpenMenu] = useState(false);
+  const open = Boolean(openMenu);
+
   return (
-    <header className='header'>
-      <h3 className='titulo'>{ titulo }</h3>
-      {auth ? (
-        <h5 className='subtitle'>{auth.nome}</h5>
-      ) : (
-        <button className='button primary'>
-          <Link to='./login' style={{color: 'white'}}>Login</Link>
-        </button>
+    <>
+      <header className='header'>
+        <h3 className='titulo'>{ titulo }</h3>
+        {auth ? (
+          <button className='header-button' onClick={() => setOpenMenu(true)}>{auth.nome}</button>
+        ) : (
+          <button className='button primary'>
+            <Link to='./login' style={{color: 'white'}}>Login</Link>
+          </button>
+        )}      
+      </header>
+      {openMenu && (
+        <Menu
+          anchorEl={openMenu}
+          open={open}
+          onClose={() => setOpenMenu(false)}
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        >
+          <MenuItem onClick={() => { localStorage.removeItem('user'); setOpenMenu(false); }}>
+            <p> Sair </p>
+          </MenuItem>
+        </Menu>
       )}
-    </header>
+    </>
   );
 }
 
